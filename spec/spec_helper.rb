13b5cef8466278a11dab50e2899fc16f0e33d617
unknown <AClark@spartaglobal.com>
2019-02-07 14:16:57 +0000

    Chef to AWS for Node/App

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+# When tests finish, generete test coverege report, i.e. what things aren't being tested
+at_exit { ChefSpec::Coverage.report! }
